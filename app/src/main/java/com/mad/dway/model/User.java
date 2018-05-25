754587c6d3dc82bfe2e0e063dbc201f08ba0db82
Alan Ng <alan.ng@blake.com.au>
2018-05-25 17:32:29 +1000

    added the user model and the repo for the users for the app

--- app/src/main/java/com/mad/dway/model/User.java
+++ app/src/main/java/com/mad/dway/model/User.java
@@ -1,8 +1,50 @@
 package com.mad.dway.model;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.Serializable;
+
 /**
  * Created by ang on 25/5/18.
  */
 
-public class User {
+public class User implements Serializable{
+    private String mName;
+    private String mEmail;
+    private LatLng mLatLng;
+
+    public User () {
+        this.mName = "";
+        this.mEmail = "";
+        this.mLatLng = null;
+    }
+
+    public User (String name, String email) {
+        this.mName = name;
+        this.mEmail = email;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public LatLng getLatLng() {
+        return mLatLng;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    public void setEmail(String email) {
+        this.mEmail = email;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        mLatLng = latLng;
+    }
 }
--- app/src/main/java/com/mad/dway/model/UserRepository.java
+++ app/src/main/java/com/mad/dway/model/UserRepository.java
@@ -1,8 +1,69 @@
 package com.mad.dway.model;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
- * Created by ang on 25/5/18.
+ * UserRepository for the User instance. Monitors for both the local and remote (Firebase)
+ * data
+ *
+ * Created by 12934713 on 25/5/18.
  */
 
 public class UserRepository {
+    private static UserRepository sInstance = null;
+    private static FirebaseDatabase mFirebaseDatabase;
+    private static DatabaseReference mUsersRef;
+    private static FirebaseAuth mFirebaseAuth;
+    private static FirebaseUser mFirebaseUser;
+    private static boolean mSignedIn;
+    private static User mLocalUser;
+    private static String mUId;
+
+    private UserRepository() {
+
+    }
+
+    public static UserRepository getInstance() {
+        if (sInstance == null) {
+            sInstance = new UserRepository();
+            mFirebaseUser = mFirebaseAuth.getInstance().getCurrentUser();
+            if (mFirebaseUser == null ) {
+                mSignedIn = false;
+            } else {
+                mSignedIn = true;
+                setUpLocalUser();
+            }
+        }
+        return sInstance;
+    }
+
+    public static void destroyInstance() {
+        sInstance = null;
+    }
+
+    private static void setUpLocalUser() {
+        mLocalUser = new User(mFirebaseUser.getDisplayName(), mFirebaseUser.getEmail());
+    }
+    
+    private static void saveLocalUserIntoFirebase() {
+        mUsersRef.child(mUId).setValue(mLocalUser);
+    }
+
+    public static void onSignUp() {
+
+    }
+
+    public static boolean isSignedIn() {
+        return mSignedIn;
+    }
+
+    public static void onSignIn() {
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mUId = mFirebaseUser.getUid();
+
+    }
+
 }
