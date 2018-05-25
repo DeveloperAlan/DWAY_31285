61312c064c8c88f5303af66197ece3b42bc20dcd
Alan Ng <alan.ng@blake.com.au>
2018-05-25 17:25:43 +1000

    added AccountsPresenter

---
+++ app/src/main/java/com/mad/dway/model/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mad.dway.model;
+
+/**
+ * Created by ang on 25/5/18.
+ */
+
+public class UserRepository {
+}
--- app/src/main/java/com/mad/dway/presenter/AccountsPresenter.java
+++ app/src/main/java/com/mad/dway/presenter/AccountsPresenter.java
@@ -1,6 +1,14 @@
 package com.mad.dway.presenter;
 
+import android.support.annotation.NonNull;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.mad.dway.view.fragments.AccountsFragment;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by ang on 21/5/18.
@@ -8,8 +16,31 @@ import com.mad.dway.view.fragments.AccountsFragment;
 
 public class AccountsPresenter {
     private AccountsFragment mAccountsView;
+    private List<AuthUI.IdpConfig> providers;
 
     public AccountsPresenter(AccountsFragment view) {
         mAccountsView = view;
+
+        providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+    }
+
+    public void createFirebaseAccountsUI() {
+        mAccountsView.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                mAccountsView.RC_SIGN_IN);
+    }
+
+
+    public void logOutFirebaseAccounts() {
+        AuthUI.getInstance()
+                .signOut(mAccountsView.getContext())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        mAccountsView.setNotLoggedInLayoutVisible();
+                    }
+                });
     }
 }
