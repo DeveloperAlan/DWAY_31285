d4b696f335f98e888614bf61db2ccdd72a541222
Alan Ng <alan.ng@blake.com.au>
2018-06-01 22:46:00 +1000

    added the presenter class for the journey presenter

---
+++ app/src/main/java/com/mad/dway/presenter/JourneyPresenter.java
@@ -0,0 +1,83 @@
+package com.mad.dway.presenter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.mad.dway.R;
+import com.mad.dway.model.PlacesEndPoint;
+import com.mad.dway.view.fragments.JourneyFragment;
+import com.mad.dway.view.fragments.SearchResultsFragment;
+
+/**
+ * Created by ang on 28/5/18.
+ */
+
+public class JourneyPresenter {
+
+    private JourneyFragment mJourneyFragment;
+    private FragmentManager mFragmentManager;
+    private PlacesEndPoint mPlacesApiEndPoint;
+    private Fragment mSearchResult;
+    private EditText mSearchEditText;
+    private View mView;
+    private Button mSearchButton;
+
+    public JourneyPresenter(JourneyFragment view) {
+        mJourneyFragment = view;
+        mFragmentManager = mJourneyFragment.getFragmentManager();
+        mView = mJourneyFragment.getView();
+        mPlacesApiEndPoint = PlacesEndPoint.getInstance();
+        mSearchEditText = mView.findViewById(R.id.fragment_journey_search_edit_text);
+        mSearchButton = mView.findViewById(R.id.destination_search_button);
+        addSearchButtonListener();
+    }
+
+    public void addResultsDropdown() {
+        if (checkIfResultsExist()) {
+            removeResultsDropdown();
+        }
+        FragmentTransaction fragTransaction = mFragmentManager.beginTransaction();
+
+        mSearchResult = new SearchResultsFragment();
+        fragTransaction.add(R.id.linear_layout_search_result, mSearchResult , "search_results");
+        fragTransaction.commit();
+    }
+
+    public void removeResultsDropdown() {
+        if (!checkIfResultsExist()) return;
+        Fragment fragment = mFragmentManager.findFragmentById(R.id.linear_layout_search_result);
+        FragmentTransaction fragTransaction = mFragmentManager.beginTransaction();
+
+        fragTransaction.remove(fragment).commit();
+    }
+
+    public boolean checkIfResultsExist() {
+        Fragment fragment = mFragmentManager.findFragmentById(R.id.linear_layout_search_result);
+        if (fragment == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public void searchForResults() {
+        mPlacesApiEndPoint.searchPlace(String.valueOf(mSearchEditText.getText()));
+        addResultsDropdown();
+    }
+
+    public void addSearchButtonListener() {
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchForResults();
+            }
+        });
+    }
+
+}
